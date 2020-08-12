85b969c3cd503c71ebd882c74e065f67a647f7da
Manuel Quarneti <manuelquarneti@gmail.com>
2020-08-12 21:48:23 +0200

    Added @types/nuxtjs__auth

---
+++ pages/login.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  mounted() {
+    if (!this.$auth.loggedIn) this.$auth.loginWith("spotify");
+  },
+});
+</script>
