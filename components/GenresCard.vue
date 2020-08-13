169e7cf936e57a154f878aad52c426d98d0b635a
Manuel Quarneti <manuelquarneti@gmail.com>
2020-08-13 19:13:22 +0200

    Recent genres

--- components/GenresCard.vue
+++ components/GenresCard.vue
@@ -1,8 +1,16 @@
 <template>
-    <v-card>
-        <v-card-title class="headline">
-            Your favorite genres
-        </v-card-title>
-        <GenresChart />
-    </v-card>
+    <div>
+        <v-card>
+            <v-card-title class="headline">
+                Your favorite genres
+            </v-card-title>
+            <GenresChart type="top" />
+        </v-card>
+        <v-card>
+            <v-card-title class="headline">
+                Your recent genres
+            </v-card-title>
+            <GenresChart type="recent" />
+        </v-card>
+    </div>
 </template>
--- components/GenresChart.ts
+++ components/GenresChart.ts
@@ -1,20 +1,50 @@
-import Vue from "vue";
-import { Doughnut, mixins } from "vue-chartjs";
-import { Component } from 'vue-property-decorator';
+import Vue, { PropOptions } from "vue";
+import { Doughnut } from "vue-chartjs";
+import { Component, Prop } from 'vue-property-decorator';
 
 const limit = 10;
+
+const backgroundColor = [
+  "rgba(255, 221, 0, 0.8)",
+  "rgba(255, 200, 0, 0.8)",
+  "rgba(255,166,0, 0.8)",
+  "rgba(255,124,67, 0.8)",
+  "rgba(249,93,106, 0.8)",
+  "rgba(212,80,135, 0.8)",
+  "rgba(160,81,149, 0.8)",
+  "rgba(102,81,145, 0.8)",
+  "rgba(47,75,124, 0.8)",
+  "rgba(29, 52, 130, 0.8)",
+  "rgba(10, 36, 66, 0.8)",
+]
+const borderColor = [
+  "rgba(255, 221, 0, 1)",
+  "rgba(255, 200, 0, 1)",
+  "rgba(255,166,0, 1)",
+  "rgba(255,124,67, 1)",
+  "rgba(249,93,106, 1)",
+  "rgba(212,80,135, 1)",
+  "rgba(160,81,149, 1)",
+  "rgba(102,81,145, 1)",
+  "rgba(47,75,124, 1)",
+  "rgba(29, 52, 130, 1)",
+  "rgba(10, 36, 66, 1)",
+]
 
 @Component({
   extends: Doughnut,
 })
 export default class GenresChart extends Vue {
+  @Prop({ type: String, required: true }) readonly type!: string
+
   public renderChart!: (chartData: any, options: any) => void;
 
   private async getGenres(url: string) {
     let genres: { name: string; score: number }[] = [];
 
-    const tracks: { items: { artists: { id: string }[] }[] } = await this.$axios.$get(url);
-    for (const track of tracks.items) {
+    const tracks: { items: any[] } = await this.$axios.$get(url);
+    for (let track of tracks.items) {
+      if (track.track) track = track.track // recently-played fix (the track object comes inside a "track" object)
       for (const artist of track.artists) {
         let artistData: { genres: string[] } = await this.$axios.$get(`https://api.spotify.com/v1/artists/${artist.id}`)
         for (const genre of artistData.genres) {
@@ -26,71 +56,37 @@ export default class GenresChart extends Vue {
       }
     }
 
+    // sort array
+    genres.sort((a, b) => b.score - a.score);
+
     return genres
   }
 
   async mounted() {
-    let genres: { name: string; score: number }[] = await this.getGenres("https://api.spotify.com/v1/me/top/tracks")
-
-    // sort array
-    genres.sort((a, b) => b.score - a.score);
+    let genres = await this.getGenres("https://api.spotify.com/v1/me/top/tracks")
 
     // get only a few genres
-    let bestGenres = genres.slice(0, limit);
+    genres = genres.slice(0, limit);
 
-    let formattedData: { labels: string[]; data: number[] } = {
-      labels: [],
-      data: [],
-    };
+    let labels: string[] = []
+    let scores: number[] = []
 
-    // add the top genres to formattedData
-    bestGenres.forEach((genre) => {
-      formattedData.labels.push(genre.name);
-      formattedData.data.push(genre.score);
-    });
 
-    // add the least favorite genres to the "other" category
-    if (genres.length > limit) {
-      let sum = 0;
-      for (let i = limit; i < genres.length; i++) sum += genres[i].score;
-
-      formattedData.labels.push("others");
-      formattedData.data.push(sum);
-    }
+    let formattedGenres = await this.getGenres(this.type === "top" ? "https://api.spotify.com/v1/me/top/tracks" : "https://api.spotify.com/v1/me/player/recently-played")
+    formattedGenres.forEach((genre: { name: string, score: number }) => {
+      labels.push(genre.name)
+      scores.push(genre.score)
+    })
 
     this.renderChart({
-      labels: formattedData.labels,
+      labels: labels,
       datasets: [
         {
-          label: "Your favorite genres",
-          data: formattedData.data,
-          backgroundColor: [
-            "rgba(255, 221, 0, 0.8)",
-            "rgba(255, 200, 0, 0.8)",
-            "rgba(255,166,0, 0.8)",
-            "rgba(255,124,67, 0.8)",
-            "rgba(249,93,106, 0.8)",
-            "rgba(212,80,135, 0.8)",
-            "rgba(160,81,149, 0.8)",
-            "rgba(102,81,145, 0.8)",
-            "rgba(47,75,124, 0.8)",
-            "rgba(29, 52, 130, 0.8)",
-            "rgba(10, 36, 66, 0.8)",
-          ],
-          borderColor: [
-            "rgba(255, 221, 0, 1)",
-            "rgba(255, 200, 0, 1)",
-            "rgba(255,166,0, 1)",
-            "rgba(255,124,67, 1)",
-            "rgba(249,93,106, 1)",
-            "rgba(212,80,135, 1)",
-            "rgba(160,81,149, 1)",
-            "rgba(102,81,145, 1)",
-            "rgba(47,75,124, 1)",
-            "rgba(29, 52, 130, 1)",
-            "rgba(10, 36, 66, 1)",
-          ],
-        },
+          label: this.type === "top" ? "Your favorite genres" : "Your recent genres",
+          data: scores,
+          backgroundColor: backgroundColor,
+          borderColor: borderColor
+        }
       ],
     }, {});
   }
