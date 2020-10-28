07c5db85952e189cabc7cd691644bf1cd07bc19e
Manuel Quarneti <manuelquarneti@gmail.com>
2020-10-28 20:43:53 +0100

    cleaner layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,11 @@
 <template>
   <div>
-    <b-navbar>
-      <template slot="brand" v-if="$auth.loggedIn">
-        <b-navbar-item>
-          <b-image :src="$auth.user.images[0].url" />
-        </b-navbar-item>
-      </template>
-      <template slot="end">
-        <b-navbar-item href="https://github.com/quarno/stop">
-          <b-icon icon="github" />
-        </b-navbar-item>
-      </template>
-    </b-navbar>
-
     <nuxt />
 
     <footer class="footer">
       <div class="content has-text-centered">
         <p>© 2020 Manuel Quarneti</p>
+        <p><a href="https://github.com/quarno/stop" target="_blank"><b-icon icon="github" /></a></p>
       </div>
     </footer>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <section class="hero is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title" v-text="$auth.user.display_name + '\'s stats'" />
+        </div>
+      </div>
+    </section>
+
     <section class="section">
       <div class="container">
         <div class="tile is-ancestor">
