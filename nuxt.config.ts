e42215595789987ab5fa366a84295cd25a1a022b
Manuel Quarneti <manuelquarneti@gmail.com>
2020-10-31 16:12:47 +0100

    re-enable icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,7 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/buefy
-    ['nuxt-buefy', { materialDesignIcons: false }],
+    'nuxt-buefy',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     '@nuxtjs/auth',
